688308842aadd902df63c278fb758612baedc258
Dylan Harrison <dylanh@uvic.ca>
2025-09-15 12:01:35 -0700

    Implemented calculator display and design w/o functionality.

--- calculator.js
+++ calculator.js
@@ -28,3 +28,37 @@ function operate(op, a, b) {
             return divide(a,b);
     }
 }
+
+//create HTML for the calculator display
+const container = document.querySelector(".container");
+
+const calcBox = document.createElement("div");
+calcBox.classList.add("calcBox");
+container.appendChild(calcBox);
+
+const display = document.createElement("div");
+display.classList.add("display");
+calcBox.appendChild(display);
+
+const buttons = [
+  '7', '8', '9', '/',
+  '4', '5', '6', '*',
+  '1', '2', '3', '-',
+  '0', '.', '=', '+',
+  'C'
+];
+
+let btnIndex = 0;
+for (let i = 0; i < 5; i++) {
+  const row = document.createElement('div');
+  row.classList.add('flex-row');
+  for (let j = 0; j < 4 && btnIndex < buttons.length; j++) {
+    const btn = document.createElement('button');
+    btn.classList.add('button'); 
+    btn.textContent = buttons[btnIndex];
+    // add event listueners, classes, etc.
+    row.appendChild(btn);
+    btnIndex++;
+  }
+  calcBox.appendChild(row);
+}
